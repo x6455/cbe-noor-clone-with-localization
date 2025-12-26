e704c73f90f34d73d59066a2861afe00c07e70ab
X <129300554+x6455@users.noreply.github.com>
2025-12-25 22:12:28 -0800

    Update login_page.dart

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -142,20 +142,20 @@ class _LoginPageState extends State<LoginPage> {
                             }
                           },
                           style: TextButton.styleFrom(
-                              elevation: 6,
-                              padding: EdgeInsets.only(
-                                  top: 20.r,
-                                  bottom: 20.r,
-                                  left: 21.r,
-                                  right: 19.r),
-                              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                              minimumSize: Size.zero,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(50.r)),
-                              backgroundColor: secondaryColor,
-                              primary: Colors.white),
-                          child: Icon(Icons.arrow_forward_ios, size: 18.sp),
-                        ),
+  elevation: 6,
+  padding: EdgeInsets.only(
+      top: 20.r,
+      bottom: 20.r,
+      left: 21.r,
+      right: 19.r),
+  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+  minimumSize: Size.zero,
+  shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(50.r)),
+  backgroundColor: secondaryColor,
+  foregroundColor: Colors.white,
+),
+
                       ),
                     ],
                   ),
